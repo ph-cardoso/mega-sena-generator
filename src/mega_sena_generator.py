60eb2b63e337d4354f5873dc95b431d9d6703934
ph-cardoso <phlc.cardoso@gmail.com>
2022-12-18 02:06:20 -0300

    fix: Fix default output filename
    Changed "/" separator to "-"

--- src/mega_sena_generator.py
+++ src/mega_sena_generator.py
@@ -11,7 +11,7 @@ Usage:
 Options:
   -q, --quantidade:  quantidade de jogos.                                   [default: 1]
   -t, --tamanho:     tamanho do jogo [6, 7, 8, 9, 10, 11, 12, 13, 14, 15].  [default: 6]
-  -o, --output:      nome do arquivo de saída.                              [default: output-{1}.txt]
+  -o, --output:      nome do arquivo de saída.                              [default: output-current_date.txt]
 
 Example:
   python mega_sena_generator.py -q 10 -t 6 -o output.txt
@@ -58,7 +58,7 @@ def getArgs(argv: list) -> dict:
     \t[default: 6]\
     \n  -o, --output:\t\t nome do arquivo de saída.\
     \t\t\t\t\t[default: output-{1}.txt]\
-  ".format(argv[0] , dt.datetime.now().strftime("%d/%m/%Y"))
+  ".format(argv[0] , dt.datetime.now().strftime("%d-%m-%Y"))
 
   try:
     opts, args = getopt.getopt(argv[1:], "hq:t:o:", ["help", "quantidade=", "tamanho=", "output="])
@@ -83,12 +83,90 @@ def getArgs(argv: list) -> dict:
   return args_dict
 
 
+def generateGames(quantidade: int, tamanho: int) -> list:
+  """Gera uma lista de jogos aleatórios com a quantidade e tamanho especificado
+  :param quantidade: quantidade de jogos
+  :param tamanho: tamanho do jogo
+  :return: lista de jogos
+
+  :Example:
+
+  >>> generateGames(1, 6)
+  [['01', '02', '03', '04', '05', '06']]
+  >>> generateGames(2, 6)
+  [['01', '02', '03', '04', '05', '06'], ['01', '02', '03', '04', '05', '06']]
+  >>> generateGames(1, 7)
+  [['01', '02', '03', '04', '05', '06', '07']]
+  """
+  games = []
+  for i in range(quantidade):
+    game = []
+
+    for j in range(tamanho):
+      number = random.randint(1, 60)
+
+      while number in game:
+        number = random.randint(1, 60)
+
+      game.append(number)
+
+    game.sort()
+    games.append(game)
+
+  returnGames = []
+  for game in games:
+    returnGames.append(["{:02d}".format(number) for number in game])
+
+  return returnGames
+
+
+def saveGames(games: list, arquivo_output: str) -> None:
+  """Salva os jogos em um arquivo de texto com o nome especificado no parâmetro arquivo_output
+  :param games: lista de jogos
+  :param arquivo_output: nome do arquivo de saída
+
+  :Example:
+
+  >>> saveGames([['01', '02', '03', '04', '05', '06']], "output.txt")
+  Arquivo output.txt salvo com sucesso!
+  """
+  path = os.path.dirname(os.path.abspath(__file__))
+  path = os.path.dirname(path)
+  path = os.path.join(path, "output")
+  arquivo_output_with_path = os.path.join(path, arquivo_output)
+
+  with open(arquivo_output_with_path, "w") as file:
+    for game in games:
+      file.write(" - ".join(game) + "\n")
+
+  print("\n\nArquivo {} salvo com sucesso!".format(arquivo_output))
+
+
+def printGames(games: list) -> None:
+  """Imprime os jogos na tela
+  :param games: lista de jogos
+
+  :Example:
+
+  >>> printGames([['01', '02', '03', '04', '05', '06'], ['01', '02', '03', '04', '05', '06']])
+
+  Jogos gerados:
+
+    01 - 02 - 03 - 04 - 05 - 06
+    01 - 02 - 03 - 04 - 05 - 06
+
+  """
+
+  print("\nJogos gerados:\n")
+  for game in games:
+    print(" - ".join(game))
+
 
 def main():
   """Função principal do programa"""
   arg_quantidade = "1"
   arg_tamanho = "6"
-  arg_arquivo_output = "output-{}.txt".format(dt.datetime.now().strftime("%d/%m/%Y"))
+  arg_arquivo_output = "output-{}.txt".format(dt.datetime.now().strftime("%d-%m-%Y"))
 
   args = getArgs(sys.argv)
 
